e135a05a9bb852823e27d6d30180a430ba9d3fcf
Supardi G <pardiwan267@gmail.com>
2023-03-31 14:33:47 +0700

    tambah tugas sesi 7

---
+++ 7 - stack/MyApp/src/BookStackApp.java
@@ -0,0 +1,87 @@
+import java.io.Console;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Stack;
+
+public class BookStackApp {
+  public static void main(String[] args){
+    Console console = System.console();
+    Stack<Book> books = new Stack<Book>();
+
+    while(true) {
+      System.out.println("\nInput buku");
+
+      System.out.print("Judul: ");
+      String title = console.readLine();
+
+      System.out.print("Penulis: ");
+      String author = console.readLine();
+
+      System.out.print("Penerbit: ");
+      String publisher = console.readLine();
+
+      System.out.print("Tahun rilis: ");
+      int year = Integer.parseInt(console.readLine());
+
+      System.out.println("\npilih kategori buku:");
+      int no = 1;
+      for (String category: Book.CATEGORIES){
+        System.out.println(no + ". " + category);
+        no++;
+      }
+      System.out.print("pilih (1-" + Book.CATEGORIES.length + "): ");
+      String category = Book.CATEGORIES[Integer.parseInt(console.readLine()) - 1];
+
+      books.add(new Book(title, category, author, publisher, year));
+
+      System.out.println("\n=============================BOOKS=========================================");
+      System.out.println("No\tTitle\t\t\tAuthor\t\tCategory\t\tPublisher\t\tRelease year");
+      System.out.println("===========================================================================");
+      no = 1;
+      for (Book book: books){
+        System.out.println(no + "\t" + book.title + "\t\t\t" + book.author + "\t\t" + book.category + "\t\t\t" + book.publisher + "\t\t\t" + book.year);
+        no++;
+      }
+      System.out.println("===========================================================================");
+
+      System.out.print("\ninput lagi (ya/tidak)?: ");
+      if (!console.readLine().equals("ya")){
+        break;
+      }
+    }
+
+    // buku yang dikelompokan oleh kategori
+    HashMap<String, ArrayList<Book>> groupedBooks = new HashMap<>();
+    for (Book book: books){
+      ArrayList<Book> groupedBook = groupedBooks.get(book.category);
+      if (groupedBook == null){
+        groupedBooks.put(book.category, new ArrayList<>(Arrays.asList(new Book[] { book })));
+      }else{
+        groupedBook.add(book);
+      }
+    }
+
+    int totalOldBooks = 0;
+    int totalNewBooks = 0;
+    for (Book book: books){
+      if (book.year < 2010){
+        totalOldBooks++;
+      }else{
+        totalNewBooks++;
+      }
+    }
+
+    System.out.println("Total buku: " + books.size());
+
+    for (String category: Book.CATEGORIES){
+      ArrayList<Book> groupedBook = groupedBooks.get(category);
+      if (groupedBook == null){
+        continue;
+      }
+      System.out.println(category + ": " + groupedBooks.get("Teknik").size());
+    }
+    System.out.println("Buku lama (<2010): " + totalOldBooks);
+    System.out.println("Buku baru: " + totalNewBooks);
+  }
+}
---
+++ 7 - stack/MyApp/src/Tugas1.java
@@ -0,0 +1,35 @@
+import java.util.Stack;
+
+public class Tugas1 {
+    public static void main(String[] args) throws Exception {
+        Stack<String> jurusan = new Stack<String>();
+        jurusan.push("Teknik Informatika");
+        jurusan.push("Teknik Industri");
+        jurusan.push("Teknik Mesin");
+        jurusan.push("DKV");
+
+        System.out.println("Stack item \t\t : " + jurusan);
+        jurusan.pop();
+        System.out.println("Stack after pop \t : " + jurusan);
+        System.out.println("Pick \t\t\t : " + jurusan.peek());
+
+        System.out.println("Check apakah kosong? \t : " + jurusan.empty());
+        System.out.println("Check apakah kosong? \t : " + jurusan.isEmpty());
+
+        System.out.println("Lihat index ke 2\t : " + jurusan.get(2));
+        System.out.println("Lihat index pertama\t : " + jurusan.firstElement());
+        System.out.println("Lihat index terakhir\t : " + jurusan.lastElement());
+        jurusan.remove(1);
+        System.out.println("Stack items sebelum clear\t : " + jurusan);
+        jurusan.clear();
+        System.out.println("Stack items setelah clear\t : " + jurusan);
+
+        jurusan.push("Teknik Informatika");
+        jurusan.push("Teknik Industri");
+        jurusan.push("Teknik Mesin");
+        jurusan.push("DKV");
+        for (String j : jurusan){
+            System.out.println("Stack items\t : " + j);
+        }
+    }
+}
